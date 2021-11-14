e590ffa341457c0a9a334b2346fdbfe51e18510b
Aariq <scottericr@gmail.com>
2021-11-14 13:01:55 -0500

    switch to building index for the python API client download

--- R/build_index.R
+++ R/build_index.R
@@ -1,41 +1,54 @@
-library(tidyverse)
-library(glue)
-library(epwshiftr)
-library(lubridate)
-library(here)
+hist <- esgf_query(
+  activity = "CMIP",
+  variable = c("tas", "tasmin", "tasmax", "pr", "hfss", "hfls"),
+  experiment = "historical",
+  frequency = "mon",
+  source = NULL,
+  resolution = NULL
+) %>% #remove duplicated variables that appear in multiple "tables"
+  group_by(source_id, variable_id) %>%
+  slice(1) %>% ungroup()
 
-add_paths <- function(idx){
-  idx %>% 
-    mutate(download_dir = here("data_raw", experiment_id, source_id),
-           file_name = glue("{variable_id}_{experiment_id}_{source_id}_{datetime_start}_{datetime_end}.nc"),
-           full_path = here(download_dir, file_name))
-}
+hist_incomp <- 
+  count(hist, source_id) %>% #should be 6
+  filter(n !=6) %>%
+  pull(source_id) 
 
-#build CMIP6 data indexes
-#historical
-idx_historical <- 
-  init_cmip6_index(
-    activity = "CMIP",
-    variable = c("tas", "tasmin", "tasmax", "pr", "hfss", "hfls"),
-    frequency = "mon",
-    experiment = "historical",
-    variant = "r1i1p1f1"
-  ) %>% 
-  filter(source_id != "CESM2") %>% #doesn't have SSP data
-  add_paths() %>% 
-  filter(!duplicated(full_path)) %>%  #some files are in multiple "tables", this removes duplicates.
-  write_csv(here("data_raw", "metadata", "idx_hist.csv"))
+hist <- hist %>% 
+  filter(!source_id %in% hist_incomp)
 
-#ssps
-idx_ssps <-
-  init_cmip6_index(
-    activity = "ScenarioMIP",
-    variable = c("tas", "tasmin", "tasmax", "pr", "hfss", "hfls"),
-    frequency = "mon",
-    experiment = c("ssp126", "ssp245", "ssp585"),
-    variant = "r1i1p1f1"
-  ) %>%
-  filter(datetime_end <= ymd("2100-12-01")) %>% 
-  add_paths() %>% 
-  filter(!duplicated(full_path)) %>%  #some files are in multiple "tables", this removes duplicates.
-  write_csv(here("data_raw", "metadata", "idx_ssps.csv"))
+ssps <- esgf_query(
+  activity = "ScenarioMIP",
+  variable = c("tas", "tasmin", "tasmax", "pr", "hfss", "hfls"),
+  experiment = c("ssp126", "ssp245", "ssp585"),
+  frequency = "mon",
+  source = NULL,
+  resolution = NULL
+) %>% #remove duplicated variables that appear in multiple "tables"
+  group_by(experiment_id, source_id, variable_id) %>%
+  slice(1) %>% ungroup()
+
+ssps_incomp <- 
+  count(ssps, source_id) %>% #should be 6*3 = 18
+  filter(n !=18) %>%
+  pull(source_id) 
+
+ssps <- ssps %>% 
+  filter(!source_id %in% ssps_incomp)
+
+
+idx <- bind_rows(hist, ssps)
+idx_incomp <-
+  count(idx, source_id) %>%  #should be 24
+  filter(n != 24) %>% 
+  pull(source_id)
+
+idx <- idx %>% 
+  filter(!source_id %in% idx_incomp)
+
+idx %>% count(source_id)
+
+
+#11 models with all the variables for both historical and projections
+
+write_csv(idx, here("data_raw", "metadata", "cmip6_index.csv"))
--- R/download_with_python.R
+++ R/download_with_python.R
@@ -3,30 +3,74 @@ library(here)
 library(tidyverse)
 library(glue)
 library(here)
-library(snakecase)
+use_virtualenv(here("r-reticulate"))
 
 # Read in indexes
-idx_ssps <- read_csv(here("data_raw", "metadata", "idx_ssps.csv"))
-idx_historical <- read_csv(here("data_raw", "metadata", "idx_hist.csv"))
+idx <- read_csv(here("data_raw", "metadata", "cmip6_index.csv"))
 
 # Wrap python function to work in R
 source_python(here("R", "download_cmip6.py"))
 
 
-download_cmip6(
-  experiment = "historical",
-  variable = "precipitation",
-  model = "mri_esm2_0",
-  path = here("data_raw", "test.zip")
-)
+# download_cmip6(
+#   experiment_id = "historical",
+#   variable_long_name = "daily_maximum_near_surface_air_temperature",
+#   source_id = "awi_cm_1_1_mr",
+#   path = "/Users/scottericr/Documents/heliconia-cmip/data_raw/historical/awi_cm_1_1_mr/tasmax_historical_awi_cm_1_1_mr.zip"
+# )
 
+#some requests apparently take a lot longer than others
 
-idx_historical %>%
+dl_df <- idx %>% 
   group_by(experiment_id, variable_long_name, variable_id, source_id) %>% 
   count() %>% #just a way of getting only unique values
   ungroup() %>% 
   select(-n) %>% 
-  mutate(across(where(is.character), to_snake_case))
+  mutate(across(
+    where(is.character),
+    ~tolower(.x) %>% str_replace_all("[-\\s]", "_")
+  )) %>% 
+  mutate(dir = here(
+    "data_raw",
+    experiment_id,
+    source_id),
+    file_name = glue("{variable_id}_{experiment_id}_{source_id}.zip"),
+    path = here(dir, file_name))
 
-#now, make new download path column, then map download_cmip6() function across those rows.
+# View(dl_df)
 
+#did we get the snakecase conversion right?
+
+# dl_df %>% count(source_id) %>% View()
+
+#available on portal:
+on_portal <- c("access_cm2", "access_esm1-5", "awi_cm-1-1-mr", "awi_esm-1-1-lr", 
+           "bcc_csm2-mr", "bcc_esm1", "cams_csm1-0", "canesm5", "canesm5_canoe", 
+           "cesm2", "cesm2_fv2", "cesm2_waccm", "cesm2_waccm-fv2", "ciesm", 
+           "cmcc_cm2-hr4", "cmcc_cm2-sr5", "cmcc_esm2", "cnrm_cm6-1", "cnrm_cm6-1-hr", 
+           "cnrm_esm2-1", "e3sm_1-0", "e3sm_1-1", "e3sm_1-1-eca", "ec_earth3", 
+           "ec_earth3-aerchem", "ec_earth3-cc", "ec_earth3-veg", "ec_earth3-veg-lr", 
+           "fgoals_f3-l", "fgoals_g3", "fio_esm-2-0", "gfdl_esm4", "giss_e2-1-g", 
+           "giss_e2-1-h", "hadgem3_gc31-ll", "hadgem3_gc31-mm", "iitm_esm", 
+           "inm_cm4-8", "inm_cm5-0", "ipsl_cm5a2-inca", "ipsl_cm6a-lr", 
+           "kace_1-0-g", "kiost_esm", "mcm_ua-1-0", "miroc6", "miroc_es2h", 
+           "miroc_es2l", "mpi_esm-1-2-ham", "mpi_esm1-2-hr", "mpi_esm1-2-lr", 
+           "mri_esm2-0", "nesm3", "norcpm1", "noresm2_lm", "noresm2_mm", 
+           "sam0_unicon", "taiesm1", "ukesm1_0-ll")
+
+avail <- dl_df %>% count(source_id) %>% filter(source_id %in% on_portal) %>% pull(source_id)
+avail
+dl_df <- dl_df %>% 
+  filter(source_id %in% avail)
+#shit, only 9 are actually available through this portal
+
+dirs_to_make <- dl_df$dir[!map_lgl(dl_df$dir, dir.exists)]
+walk(unique(dirs_to_make), ~dir.create(.x, recursive = TRUE))
+
+
+dl_df %>%
+  select(experiment_id, variable_long_name, source_id, path) %>%
+  # head(2) %>% #for testing
+  pwalk(download_cmip6)
+
+#TODO: make download_cmip6 fail gracefully, only download files that don't already exist
