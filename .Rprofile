0b3aa4b1fc1fbce8da80686f80952bb174444653
Aariq <scottericr@gmail.com>
2021-11-14 10:57:38 -0500

    get python setup better

--- .Rprofile
+++ .Rprofile
@@ -1,3 +1,3 @@
 options(epwshiftr.dir = here::here("cache_data"))
 options(epwshiftr.verbose = TRUE)
-Sys.setenv(RETICULATE_PYTHON = "my_env/bin/python")
+Sys.setenv(RETICULATE_PYTHON = "r-reticulate/bin/python")
--- R/download_cmip6.py
+++ R/download_cmip6.py
@@ -1,15 +1,15 @@
 import cdsapi
 
-def download_cmip6(experiment, variable, model, path):
+def download_cmip6(experiment_id, variable_long_name, source_id, path):
     c = cdsapi.Client()
     data = c.retrieve(
       'projections-cmip6',
       {
           'temporal_resolution': 'monthly',
-          'experiment': experiment,
+          'experiment': experiment_id,
           'level': 'single_levels',
-          'variable': variable,
-          'model': model,
+          'variable': variable_long_name,
+          'model': source_id,
           'ensemble_member': 'r1i1p1',
           'area': [
               0, -65, -5,
